8e4a35db8cfa7deb6087e03f60ee8a89db9def93
wu kai <wukai2048@gmail.com>
2019-08-21 15:25:11 +0800

    在滑动验证码之前输入手机号

--- 05/index.js
+++ 05/index.js
@@ -24,6 +24,16 @@ async function run() {
   await page.waitFor(5000);
   await page.click('#J_AgreementBtn');
 
+  // 输入手机号
+  await page.waitFor(1000);
+  // 选择手机号前缀为中国大陆的 +86
+  await page.click('#J_MobileForm > div > div:nth-child(1) > div:nth-child(1) > div > div:nth-child(1)');
+  await page.waitFor(1000);
+  await page.click('.tb-option.tb-menuitem > span[title="中国大陆"]');
+  await page.focus('#J_Mobile');
+  await page.keyboard.type('15011111111');  // a fake phone number
+  await page.waitFor(1500);
+
   // 使用 page.$ 方法来寻找元素，返回值是一个 ElementHandle
   // 这里找到的是滑块所在的整个滑动区域
   const sliderElement = await page.$('#nc_1_wrapper');
@@ -36,7 +46,7 @@ async function run() {
   // 将滑块从左侧移至右侧
   await page.mouse.move(button.x + button.width / 2, button.y + button.height / 2);
   await page.mouse.down();
-  await page.mouse.move(button.x + slider.width, button.y + button.height / 2, { steps: 20 });
+  await page.mouse.move(button.x + slider.width, button.y + button.height / 2, { steps: 10 });
   await page.mouse.up();
 
   await page.waitFor(5000);
@@ -46,7 +56,11 @@ async function run() {
     fullPage: true
   });
 
+  // 点击下一步
+  // await page.click('#J_BtnMobileForm');
+  // await page.waitFor(5000);
+
   await browser.close();
 }
 
-run('puppeteer');
+run();
